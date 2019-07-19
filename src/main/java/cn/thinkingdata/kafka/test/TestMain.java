73b47a0f13229510d6a7cf37ab45af951d00b4d5
yangruochen <yangruochen@thinkingdata.cn>
2019-07-19 17:18:59 +0800

    kafka-mysql-offset 0.1.0 测试更改

--- src/main/java/cn/thinkingdata/kafka/test/TestMain.java
+++ src/main/java/cn/thinkingdata/kafka/test/TestMain.java
@@ -4,12 +4,15 @@ package cn.thinkingdata.kafka.test;
 import cn.thinkingdata.kafka.close.ScanTermMethod;
 import cn.thinkingdata.kafka.consumer.IDataLineProcessor;
 import cn.thinkingdata.kafka.consumer.KafkaSubscribeConsumer;
+import cn.thinkingdata.kafka.consumer.persist.DefaultStorePersist;
 import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -25,7 +28,13 @@ public class TestMain {
 
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+//		String path = System.getProperty("user.dir")
+//				+ "/src/main/resources/log4j.properties";
+		InputStream resourceAsStream = TestMain.class.getClassLoader().getResourceAsStream("log4j.properties");
 
+		Properties props = new Properties();
+	    props.load(resourceAsStream);
+		// java -jar kafka-mysql-offset-jar-with-dependencies.jar app:9092,app2:9092 app test3 testyrc 3 3 1 4 209715 500
 		String brokerList = args[0];
 		String kafkaClusterName = args[1];
 		String topic = args[2];
@@ -35,11 +44,14 @@ public class TestMain {
 		String flushInterval = args[6];
 		final String dataProcessNum = args[7];
 		String maxPartitionFetchBytes = args[8];
+		String dataMaxPollRecords = args[9];
 		
 		URL url = TestMain.class.getResource("/log4j.properties");
 		PropertyConfigurator.configure(url);
-		
-		
+
+		DefaultStorePersist defaultStorePersist = new DefaultStorePersist();
+
+
 
 		IDataLineProcessor dataProcessor = new IDataLineProcessor() {
 			
@@ -72,21 +84,21 @@ public class TestMain {
 		};
 
 
-		if(maxPartitionFetchBytes == null){
+//		if(maxPartitionFetchBytes == null){
 			consumers = new KafkaSubscribeConsumer(jdbcUrl, "ta", "TaThinkingData",
 					"kafka_consumer_offset", brokerList, kafkaClusterName, topic,
 					consumerGroup, dataProcessor, Integer.parseInt(processThreadNum),
 					Integer.parseInt(flushOffsetSize),
-					Integer.parseInt(flushInterval),new ScanTermMethod());
+					Integer.parseInt(flushInterval),3, Long.parseLong(maxPartitionFetchBytes), 3, 30, 35,
+					"earliest",new ScanTermMethod(),defaultStorePersist, Integer.parseInt(dataMaxPollRecords));
 
-		} else {
-			consumers = new KafkaSubscribeConsumer(jdbcUrl, "ta", "TaThinkingData",
-					"kafka_consumer_offset", brokerList, kafkaClusterName, topic,
-					consumerGroup, dataProcessor, Integer.parseInt(processThreadNum),
-					Integer.parseInt(flushOffsetSize),
-					Integer.parseInt(flushInterval),new ScanTermMethod());
-
-		}
+//		} else {
+//			consumers = new KafkaSubscribeConsumer(jdbcUrl, "ta", "TaThinkingData",
+//					"kafka_consumer_offset", brokerList, kafkaClusterName, topic,
+//					consumerGroup, dataProcessor, Integer.parseInt(processThreadNum),
+//					Integer.parseInt(flushOffsetSize),
+//					Integer.parseInt(flushInterval),new ScanTermMethod());
+//		}
 		
 		consumers.run();
 	}
